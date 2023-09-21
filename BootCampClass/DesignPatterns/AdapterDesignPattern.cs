18e4511a948e5f347180f5710c7b62467bf25493
KhageshorGiri <girikhageshor2432@gmail.com>
2023-09-21 21:53:13 +0545

    template design pattern

--- BootCampClass/DesignPatterns/AdapterDesignPattern.cs
+++ BootCampClass/DesignPatterns/AdapterDesignPattern.cs
@@ -6,7 +6,69 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
-    public class AdapterDesignPattern
+    interface Bird
     {
+        void MakeSound();
+        void Fly();
+    }
+
+    public class Pigeon : Bird
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Pegion can fly.");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Pegion can nake sound.");
+        }
+    }
+
+    interface Duck
+    {
+        void squeak();
+    }
+
+    public class KingDuck : Duck
+    {
+        public void squeak()
+        {
+            Console.WriteLine("King Duck can sqweak.");
+        }
+    }
+
+    class DuckAdaptor : Duck
+    {
+        Bird bird;
+        public DuckAdaptor(Bird _bird)
+        {
+            this.bird = _bird;
+        }
+        public void squeak()
+        {
+            bird.MakeSound();
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Pigeon pigeon = new Pigeon();
+            Duck kingDuck = new KingDuck();
+
+            Duck duckAdapter = new DuckAdaptor(pigeon);
+
+            Console.WriteLine("Pigeon...");
+            pigeon.Fly();
+            pigeon.MakeSound();
+
+            Console.WriteLine("King Duck...");
+            kingDuck.squeak();
+
+            Console.WriteLine("Duck Adapter...");
+            duckAdapter.squeak();
+        }
     }
 }
--- BootCampClass/DesignPatterns/TemplateDesignPattern.cs
+++ BootCampClass/DesignPatterns/TemplateDesignPattern.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DesignPatterns
 {
-    public class TemplateDesignPattern
+    internal abstract class Game
     {
+        private void Start()
+        {
+            Console.WriteLine("Game Started.");
+        }
+
+        protected abstract void Play();
+
+        private void Stop()
+        {
+            Console.WriteLine("Game Finished.");
+        }
+
+        public void GamePlay()
+        {
+            Start();
+            Play();
+            Stop();
+        }
+    }
+
+    internal class FootBall : Game
+    {
+        protected override void Play()
+        {
+            Console.WriteLine("Playing Football.");
+        }
+    }
+
+    internal class Circket : Game
+    {
+        protected override void Play()
+        {
+            Console.WriteLine("Playing Circket.");
+        }
+    }
+
+    public class StartGame
+    {
+        /*public static void Main(string[] args)
+        {
+            Game gameFootball = new FootBall();
+            Game gameCricket = new Circket();
+
+            // starting football
+            gameFootball.GamePlay();
+            gameCricket.GamePlay();
+            
+
+            Console.ReadLine();
+        }*/
     }
 }
